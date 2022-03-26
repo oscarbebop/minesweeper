cd24f957bbf484e5c0098d70faa5f21b3c0635a1
Oscar <oscar@bigzeta.com>
2022-03-26 17:31:03 -0600

    first stories

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
+import { Legend } from "./components/Top/Legend";
+
 export function App(): JSX.Element {
   return (
     <div>
+      <Legend />
       <p>App!</p>
     </div>
   );
